150fb913400dcc6dd94cce694d0dd6e4e3410605
gaurav pathak <gspat27@gmail.com>
2019-01-05 19:10:35 -0500

    Update wrapper.py
    I believe this should be a property, to make it compatible with gym wrapper

--- robosuite/wrappers/wrapper.py
+++ robosuite/wrappers/wrapper.py
@@ -39,9 +39,10 @@ class Wrapper:
 
     def observation_spec(self):
         return self.env.observation_spec()
-
+    
+    @property
     def action_spec(self):
-        return self.env.action_spec()
+        return self.env.action_spec
 
     @property
     def dof(self):
